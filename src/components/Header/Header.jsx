003d21e41bc3249abb98d0c1b2306e904488c63d
miguel.dinois <miguel.dinois@aluno.senai.br>
2026-08-27 08:11:32 -0300

    feat: importação do arquivo de variaveis globais de estilos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import styles from './Header.module.css';
-
+import '../../styles/variables.css';
 import logoImg from '../../assets/logo.svg';
 import sunImg from '../../assets/sun.svg';
 import moonImg from '../../assets/moon.svg';
@@ -9,7 +9,9 @@ import moonImg from '../../assets/moon.svg';
 export default function Header() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  
   const toggleTheme = () => {
+    // Definindo o proximo tema, que no caso é o tema diferente do atual 
     const nextTheme = !isDarkMode;
     setIsDarkMode(nextTheme);
     
@@ -22,26 +24,26 @@ export default function Header() {
   return (
     <header className={`d-flex justify-content-between align-items-center py-3 px-4 ${styles.headerContainer}`}>
       
-      {/* Logo e Título */}
+      {/* Logo e titulo */}
       <div className="d-flex align-items-center gap-3">
         <img 
           src={logoImg} 
           alt="Logo Jogo da Velha" 
           className={styles.logo} 
         />
-        <h1 className={`m-0 h4 ${styles.title}`}>Jogo da Velha</h1>
+        <h1 className={`m-0 h4 ${styles.titulo}`}>Jogo da Velha</h1>
       </div>
 
       {/* Botão Tema*/}
       <button 
         onClick={toggleTheme}
-        className={`btn d-flex align-items-center justify-content-center p-2 ${styles.themeBtn}`}
+        className={`btn d-flex align-items-center justify-content-center p-2 ${styles.botaoTema}`}
         aria-label="Alternar tema de cores"
       >
         <img 
           src={isDarkMode ? sunImg : moonImg} 
           alt={isDarkMode ? "Ativar Modo Claro" : "Ativar Modo Escuro"} 
-          className={styles.themeIcon}
+          className={styles.temaIcone}
         />
       </button>
 
